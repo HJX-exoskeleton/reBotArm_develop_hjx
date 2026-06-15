3aa68c598d2de4445a6ac994a2b24254bba07d7c
HJX-exoskeleton <2545304960@qq.com>
2026-06-15 20:57:42 +0800

    servo control

--- master_slave_control/Servo_control/data/read_rebot_hdf5.py
+++ master_slave_control/Servo_control/data/read_rebot_hdf5.py
@@ -2,13 +2,13 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 
-path = '/home/hjx/hjx_file/rebot_devarm_ws/reBotArm_develop_hjx/master_slave_control/Servo_control/data/control_test/episode_0.hdf5'
+path = '/home/hjx/hjx_file/rebot_devarm_ws/reBotArm_develop_hjx/master_slave_control/Servo_control/data/rebot_test/episode_0.hdf5'
 
 obj = h5py.File(path)
 print(obj.keys())
 print(obj['action'])
-# print(obj['action'].keys())
-# print(obj['action']['target_pos'])
+print(obj['action'].keys())
+print(obj['action']['target_pos'])
 print('-------------------------')
 print(obj['observations'])
 print(obj['observations'].keys())
